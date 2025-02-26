3606b631cdee158b786b3eb4f4ca609dd93a9024
tanbirali <tanbirali2004@gmail.com>
2025-02-26 22:05:35 +0530

    generate referral code updated

--- controller/generateReferral.controller.js
+++ controller/generateReferral.controller.js
@@ -1,3 +1,5 @@
-export function generateReferralCode() {
+function generateReferralCode() {
   return Math.random().toString(36).substring(2, 8).toLowerCase();
 }
+
+module.exports = { generateReferralCode };
